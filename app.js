4b7263178cd68874346dfc1c3b1e2999aa7a54fb
naavika <niksshahiruchi@gmail.com>
2024-03-27 16:04:30 +0530

    express

--- app.js
+++ app.js
@@ -1,48 +1,15 @@
-const http = require('http');
-const routes = require('./routes');
-console.log(routes.someText);
-// const fs = require('fs');
+const express = require('express');
 
-const server = http.createServer(routes.handler);
-    // const url = req.url;
-    // const method = req.method;
-    // if(url === '/') {
-    //     res.write('<html>');
-    //     res.write('<head><title>Enter Message</title></head>');
-    //     res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-    //     res.write('<html>');
-    //     return res.end();
-    // }
-    // if(url === '/message' && method === 'POST') {
-    //     const body = [];
-    //     req.on('data', (chunk) => {
-    //         console.log(chunk);
-    //         body.push(chunk);
-    //     });
-    //     req.on('end', () => {
-    //         const parsedBody = Buffer.concat(body).toString();
-    //         const message = parsedBody.split('=')[1];
-    //         fs.writeFile('message.txt', message, err =>  {
-    //             if (err) {
-    //                 console.error(err);
-    //                 res.statusCode = 500;
-    //                 res.setHeader('Content-Type', 'text/plain');
-    //                 res.write('Error occurred while writing to file!');
-    //                 return res.end();
-    //             }
-    //             res.statusCode = 302;
-    //             res.setHeader('Location', '/');
-    //             return res.end();
-    //         });
-    //     });
-    // }
-    // res.setHeader('Content-Type', 'text/html');
-    // res.write('<html>');
-    // res.write('<head><title>My First Page</title></head>');
-    // res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    // res.write('</html>');
-    // res.end();
-// });
+const app = express();
 
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next();
+});
 
-server.listen(4000);
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(4000);
